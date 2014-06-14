6e2196cc4b353dcf8067e3dee7fdc4c120628d0f
Nite Luo <darkdukey@gmail.com>
2014-06-14 13:42:58 +0800

    Add dialog box to callbacks

--- Demo/gametest/src/com/example/gametest/MainActivity.java
+++ Demo/gametest/src/com/example/gametest/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.content.DialogInterface;
 
 import com.ckmobilling.CkSdkActivity;
 import com.ckmobilling.CkSdkApi;
@@ -11,6 +15,8 @@ import com.ckmobilling.PaymentCallback;
 import com.ckmobilling.PaymentResult;
 
 public class MainActivity extends CkSdkActivity implements OnClickListener {
+	
+	final Context ctx = this;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +33,41 @@ public class MainActivity extends CkSdkActivity implements OnClickListener {
 
 			@Override
 			public void paySuccess(PaymentResult result) {
+				AlertDialog alertDialog = new AlertDialog.Builder(ctx).create();
+				alertDialog.setTitle("Success");
+				alertDialog.setMessage("Payment Successful!");
+				alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+				// here you can add functions
+				}
+				});
+				alertDialog.show();
 			}
 
 			@Override
 			public void payFaliled(PaymentResult result) {
+				AlertDialog alertDialog = new AlertDialog.Builder(ctx).create();
+				alertDialog.setTitle("Failed");
+				alertDialog.setMessage("Payment Failed.");
+				alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+				// here you can add functions
+				}
+				});
+				alertDialog.show();
 			}
 
 			@Override
 			public void payCanceld(PaymentResult result) {
+				AlertDialog alertDialog = new AlertDialog.Builder(ctx).create();
+				alertDialog.setTitle("Cancel");
+				alertDialog.setMessage("Payment Canceled.");
+				alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+				// here you can add functions
+				}
+				});
+				alertDialog.show();
 			}
 			
 		});
